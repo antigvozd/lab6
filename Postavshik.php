d572d02166205744cd59e4a962a7dd873cc4e0fc
antigvozd <antigvozd@yandex.ru>
2016-05-27 10:06:01 +0300

    Добавление переменных класса

--- Postavshik.php
+++ Postavshik.php
@@ -2,7 +2,11 @@
 
     class Postavshik
     {
-
+        var $fio; ///<ФИО Поставщика
+        var $address; ///<Адрес Поставщика
+        var $telefon;   ///<Телефон Поставщика
+        var $cathegory; ///<Категория Поставщика
+        
         function setinfo($fio, $address, $telefon, $cathegory){
             $this->fio = $fio;
             $this->address = $address;
